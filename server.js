912621322195f4dcf97fafe62af63117ce1847ab
jamiguel23 <josematthewmiguel@gmail.com>
2021-12-07 15:13:25 -0800

    added wireframe exercise photo, created server.json, added gitignore

---
+++ server.js
@@ -0,0 +1,14 @@
+'use strict';
+
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+
+const app = express()
+
+app.use(cors());
+
+const PORT = 3001;
+
+app.listen(PORT, () => console.log("server is listening on port", PORT));
