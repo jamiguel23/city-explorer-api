1c7bf638abe3960957ea6763cb4ee4a87347ba60
jamiguel23 <josematthewmiguel@gmail.com>
2021-12-07 17:13:17 -0800

    added handle test and get weather function

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const weatherData = require('./data/weather.json');
 
 
 const app = express()
@@ -10,5 +11,18 @@ const app = express()
 app.use(cors());
 
 const PORT = process.env.PORT
+
+
+app.get('/test', handleGetTest)
+app.get('/weather', handleGetWeather);
+
+
+function handleGetWeather(req,res){
+  res.status(200).send(weatherData);
+}
+
+function handleGetTest( request, response){
+  response.send('your test worked');
+}
 
 app.listen(PORT, () => console.log("server is listening on port", PORT));
